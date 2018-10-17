773d774425caaa48035fe22f86f2c7ee6e683072
xingtingbiao <xingtingbiao@znlhzl.com>
2018-10-17 11:03:17 +0800

    feature: 自定义注解

--- sqlscandemo/core/core-impl/src/main/java/com/huawei/spring/core/implement/SQLScan/util/ExcelUtil2.java
+++ sqlscandemo/core/core-impl/src/main/java/com/huawei/spring/core/implement/SQLScan/util/ExcelUtil2.java
@@ -1,5 +1,6 @@
 //package com.huawei.spring.core.implement.SQLScan.util;
 //
+//import com.znlhzl.bi.entity.vo.ClaimReportVo;
 //import org.apache.poi.ss.usermodel.CellStyle;
 //import org.apache.poi.ss.usermodel.IndexedColors;
 //import org.apache.poi.ss.usermodel.Sheet;
@@ -13,66 +14,86 @@
 //import java.io.OutputStream;
 //import java.io.UnsupportedEncodingException;
 //import java.lang.reflect.Field;
-//import java.lang.reflect.InvocationTargetException;
+//import java.net.URLEncoder;
 //import java.text.SimpleDateFormat;
 //import java.util.Date;
 //import java.util.List;
-//import java.util.Map;
+//
 //
 //public class ExcelUtil2 {
-//    //对应poi版本为3.9
-//    public static SXSSFWorkbook getWorkBook(List<?> objects, Class c) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+//    public static SXSSFWorkbook getWorkBook(List<?> objects, Class c) throws IllegalAccessException {
 //        SXSSFWorkbook wb = new SXSSFWorkbook(1000);
 //        SXSSFSheet sheet = (SXSSFSheet) wb.createSheet("sheet1");
-//        // setWidth(sheet);
 //        SXSSFRow row = (SXSSFRow) sheet.createRow(0);
 //        row.setHeightInPoints(40);
-//        //CellStyle style = wb.createCellStyle();
 //        CellStyle style = getCellStyle(wb);
 //        style.setAlignment(CellStyle.ALIGN_CENTER);
 //        style.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
 //        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
-//        style.setFillForegroundColor(IndexedColors.CORNFLOWER_BLUE.getIndex());
-//        // Map<Integer, String> columnMap = resultColumnMap();
-//        Map<Integer, String> columnMap = (Map<Integer, String>) c.getMethod("getFieldsAlias").invoke(c.newInstance());
-//        SXSSFCell cell = null;
-//        for (Map.Entry<Integer, String> entry : columnMap.entrySet()) {
-//            cell = (SXSSFCell) row.createCell(entry.getKey());
-//            cell.setCellValue(entry.getValue());
-//            cell.setCellStyle(style);
-//        }
+//        style.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.getIndex());
+//        int n = setCellTitle(c, row, style);
 //        for (int i = 0; i < objects.size(); i++) {
 //            row = (SXSSFRow) sheet.createRow(i + 1);
 //            useReflectToGetValue(row, objects.get(i), wb);
 //        }
-//        autoSizeColumns(sheet, columnMap.size());
+//        autoSizeColumns(sheet, n);
 //        return wb;
 //    }
 //
+//    private static int setCellTitle(Class c, SXSSFRow row, CellStyle style) {
+//        // Map<Integer, String> columnMap = (Map<Integer, String>) c.getMethod("getFieldsAlias").invoke(c.newInstance());
+//        SXSSFCell cell;
+//        Field[] fields = c.getDeclaredFields();
+//        int n = 0;
+//        for (Field f : fields) {
+//            f.setAccessible(true);
+//            ExcelTitle title = f.getAnnotation(ExcelTitle.class);
+//            if (null != title) {
+//                cell = (SXSSFCell) row.createCell(n);
+//                cell.setCellValue(title.value());
+//                cell.setCellStyle(style);
+//                n++;
+//            }
+//        }
+//        return n + 1;
+//    }
 //
 //    private static void useReflectToGetValue(SXSSFRow row, Object object, SXSSFWorkbook wb) throws IllegalAccessException {
 //        Class<?> aClass = object.getClass();
 //        Field[] fields = aClass.getDeclaredFields();
 //        CellStyle style = getCellStyle(wb);
 //        SXSSFCell cell;
-//        for (int i = 0; i < fields.length; i++) {
-//            fields[i].setAccessible(true);
-//            cell = (SXSSFCell) row.createCell(i);
-//            Object value = fields[i].get(object);
-//            if (value instanceof Date) {
-//                cell.setCellValue(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(value));
-//            } else if (value == null) {
-//                cell.setCellValue("");
-//            } else {
-//                cell.setCellValue(String.valueOf(value));
+//        int n = 0;
+//        for (Field f : fields) {
+//            f.setAccessible(true);
+//            ExcelTitle title = f.getAnnotation(ExcelTitle.class);
+//            if (null != title) {
+//                cell = (SXSSFCell) row.createCell(n);
+//                Object value = f.get(object);
+//                if (flagForClaim(object, f.getName())) {
+//                    value = ClaimReportVo.statusMap().get(String.valueOf(value));
+//                }
+//                if (value instanceof Date) {
+//                    cell.setCellValue(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(value));
+//                } else if (value == null) {
+//                    cell.setCellValue("");
+//                } else {
+//                    cell.setCellValue(String.valueOf(value));
+//                }
+//                cell.setCellStyle(style);
+//                n++;
 //            }
-//            cell.setCellStyle(style);
 //        }
+//    }
+//
+//    private static boolean flagForClaim(Object o, String name) {
+//        return o instanceof ClaimReportVo && "status".equals(name);
 //    }
 //
 //    private static void autoSizeColumns(Sheet sheet, int columnNumber) {
 //        for (int i = 0; i < columnNumber; i++) {
-//            sheet.autoSizeColumn(i, true);
+//            // sheet.autoSizeColumn(i, true);
+//            sheet.setColumnWidth(i, 22 * 256);
 //        }
 //    }
 //
@@ -82,9 +103,9 @@
 //        style.setBorderLeft(CellStyle.BORDER_THIN);
 //        style.setBorderTop(CellStyle.BORDER_THIN);
 //        style.setBorderRight(CellStyle.BORDER_THIN);
-//        style.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
-//        style.setAlignment(CellStyle.ALIGN_LEFT);
-//        style.setWrapText(true);
+//        // style.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+//        // style.setAlignment(CellStyle.ALIGN_LEFT);
+//        // style.setWrapText(true);
 //        return style;
 //    }
 //
@@ -103,13 +124,13 @@
 //        return h;
 //    }
 //
-//    public static void export(HttpServletResponse response, SXSSFWorkbook workBook, String fileName) throws UnsupportedEncodingException {
-//        String format = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date());
-//        String name = fileName + "_" + format + ".xlsx";
+//    public static void export(HttpServletResponse response, SXSSFWorkbook workBook, String name) throws UnsupportedEncodingException {
+//        String format = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
+//        String fileName = URLEncoder.encode(name + "_" + format + ".xlsx", "utf-8");
 //        // response.setHeader("Accept-Ranges","bytes");
 //        // response.setHeader("ETag", "");
 //        response.setContentType("application/octet-stream");
-//        response.setHeader("Content-Disposition", "attachment;filename=" + new String(name.getBytes("UTF-8"), "iso-8859-1"));
+//        response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
 //        OutputStream os = null;
 //        try {
 //            os = response.getOutputStream();
