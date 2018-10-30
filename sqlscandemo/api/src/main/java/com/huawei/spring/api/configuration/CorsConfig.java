23480e3c3c29cb8a7a6467cd30a2fc49ad8af6c6
xingtingbiao <xingtingbiao@znlhzl.com>
2018-10-30 17:25:41 +0800

    feature: 自定义注解

--- sqlscandemo/api/src/main/java/com/huawei/spring/api/configuration/CorsConfig.java
+++ sqlscandemo/api/src/main/java/com/huawei/spring/api/configuration/CorsConfig.java
@@ -2,10 +2,14 @@ package com.huawei.spring.api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by xWX522916 on 2017/11/24.
@@ -35,6 +39,16 @@ public class CorsConfig {
     scheduler.setRemoveOnCancelPolicy(true);
     scheduler.initialize();
     return scheduler;
+  }
+
+
+  // 常用： 秒、分、时、日、月、年
+  // 0 0 10,14,16 * * ? 每天上午10点，下午2点，4点
+  // 0 0 12 * * ? 每天中午12点触发
+  // 0 0/5 0 * * ? 每5分钟执行一次
+  @Scheduled(cron = "0/5 * * * * ?")
+  public void testSchedule() {
+    System.out.println("当前时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
   }
 }
 
