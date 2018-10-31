f33961fff3f17b9210c370df9075bf2a7a167393
xingtingbiao <xingtingbiao@znlhzl.com>
2018-10-31 14:11:35 +0800

    feature: 自定义注解

--- sqlscandemo/api/src/main/java/com/huawei/spring/api/configuration/CorsConfig.java
+++ sqlscandemo/api/src/main/java/com/huawei/spring/api/configuration/CorsConfig.java
@@ -46,7 +46,8 @@ public class CorsConfig {
   // 0 0 10,14,16 * * ? 每天上午10点，下午2点，4点
   // 0 0 12 * * ? 每天中午12点触发
   // 0 0/5 0 * * ? 每5分钟执行一次
-  @Scheduled(cron = "0/5 * * * * ?")
+  // @Scheduled(cron = "0/5 * * * * ?")
+  @Scheduled(cron = "0 0/1 * * * ?")
   public void testSchedule() {
     System.out.println("当前时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
   }
